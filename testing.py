b0fd6c5c06ff274a0402585298b0fb5eaf2354ae
Gustavo <riquesgustavo@gmail.com>
2023-01-09 07:37:44 -0300

    For convinience, allowed the number input with ','

--- testing.py
+++ testing.py
@@ -3,18 +3,20 @@ from random import randrange
 import decimal
 itens = input()
 itens = itens.split('/')
+for i in range(1, len(itens)):
+    itens[i] = itens[i].replace(",", ".")
+print(itens)
 aList = []
 
 for i in range(1, len(itens)):
     decimals = int(len(itens[i].split('.')[1]))
     for j in range(3):
         if j == 0:
-            aList.append(float(itens[i]))
+            aList.append(round(float(itens[i]), decimals))
         else:
             randomFactor = randrange(0,10)
             randomFactor = (randomFactor/(10**(decimals-1)))
             x = random.uniform(-randomFactor, randomFactor)
-            print(x)
             y = float(itens[i])
             aList.append(round((x+y), decimals))
-    print(aList)
+    #print(aList)
